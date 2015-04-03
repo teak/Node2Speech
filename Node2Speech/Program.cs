e38b4b80b80abcd849bc32a6936b581fe99028b5
teak <teaksoftware@gmail.com>
2015-04-03 10:05:51 -0700

    added gender selection

--- Node2Speech/Program.cs
+++ Node2Speech/Program.cs
@@ -12,16 +12,39 @@ namespace Node2Speech
     {
         static void Main(string[] args)
         {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
             if (args.Length > 2)
             {
-                SpeechSynthesizer synthesizer = new SpeechSynthesizer();
                 synthesizer.Volume = Int32.Parse(args[0]);
                 synthesizer.Rate = Int32.Parse(args[1]);
 
-                string[] speak = new string[args.Length - 2];
-                Array.Copy(args, 2, speak, 0, args.Length - 2);
+                var gender = VoiceGender.Male;
+                if (args[2] == "female")
+                {
+                    gender = VoiceGender.Female;
+                }
 
-                synthesizer.Speak(string.Join(" ", speak));
+                synthesizer.SelectVoiceByHints(gender);
+                synthesizer.SetOutputToDefaultAudioDevice();
+
+                string[] speak = new string[args.Length - 3];
+                Array.Copy(args, 3, speak, 0, args.Length - 3);
+
+                PromptBuilder builder = new PromptBuilder();
+                builder.AppendText(string.Join(" ", speak));
+
+                synthesizer.Speak(builder);
+            }
+            else if (args.Length == 1)
+            {
+                if (args[0] == "list")
+                {
+                    foreach (var v in synthesizer.GetInstalledVoices().Select(v => v.VoiceInfo))
+                    {
+                        Console.WriteLine("{0},{1},{2}", v.Description, v.Gender, v.Age);
+                    }
+                }
             }
         }
     }
--- README.md
+++ README.md
@@ -9,8 +9,13 @@ Requires three or more command line arguments:
 
 1. The output volume. Between 0 and 100.
 2. The output speed. Between -10 and 10.
+3. A gender. If set to anything but female, a male voice will be used.
 3. The text to speak. Any farther args are joined and spoken as well.
 
-All other options for speech are set system wide, using the Control Panel.
+If run with the single arg 'list' it outputs installed voice info to stdout. Example output:
+
+Microsoft Hazel Desktop - English (Great Britain),Female,Adult
+
+A one per line, comma seperated list. ("Description,Gender,Age")
 
 MIT License
